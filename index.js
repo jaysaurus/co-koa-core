688f1dc7fcce622c872bd0bbf7b23f4e33637c30
Jaysaurus <the.jaysaurus@gmail.com>
2018-01-21 23:28:12 +0000

    updated index.js to simpler stampit style

--- index.js
+++ index.js
@@ -30,48 +30,46 @@ module.exports = stampit({
     const middleware = require(`${root}/config/middleware`);
     const conf = ClientConfigFactory(root).build(environment);
 
-    Object.assign(this, {
-      launch (...plugins) {
-        const app = new Koa().use(BodyParser());
-        const router = new Router();
-        WelcomeMessage(conf).sayHello();
-        const $ = DependencyManager(conf);
+    this.launch = function (...plugins) {
+      const app = new Koa().use(BodyParser());
+      const router = new Router();
+      WelcomeMessage(conf).sayHello();
+      const $ = DependencyManager(conf);
 
-        /*
-        * SETUP MODELS
-        */
-        ModelFactory(conf).build($.call);
+      /*
+      * SETUP MODELS
+      */
+      ModelFactory(conf).build($.call);
 
-        /*
-        * PLUGINS
-        */
-        if (plugins.length) {
-          PluginManager(conf).init(app, $.call, ...plugins);
-        }
-        /*
-        * SETUP MIDDLEWARE
-        */
-        BuiltInMiddleware(app, conf).build();
-        const wares = Object.assign({}, middleware($.call));
-        Object.keys(wares).forEach(key => { app.use(wares[key]); });
-
-        /*
-        * BUILD CONTROLLERS
-        */
-        ControllerFactory(conf).build(router, $);
-
-        /*
-        * BOOTSTRAP
-        */
-        require(`${root}/config/bootstrap`).bootstrap($.call);
-
-        conf.logger.log(`listening on port ${conf.env.port}`);
-        return {
-          app,
-          port: conf.env.port,
-          router
-        };
+      /*
+      * PLUGINS
+      */
+      if (plugins.length) {
+        PluginManager(conf).init(app, $.call, ...plugins);
       }
-    });
+      /*
+      * SETUP MIDDLEWARE
+      */
+      BuiltInMiddleware(app, conf).build();
+      const wares = Object.assign({}, middleware($.call));
+      Object.keys(wares).forEach(key => { app.use(wares[key]); });
+
+      /*
+      * BUILD CONTROLLERS
+      */
+      ControllerFactory(conf).build(router, $);
+
+      /*
+      * BOOTSTRAP
+      */
+      require(`${root}/config/bootstrap`).bootstrap($.call);
+
+      conf.logger.log(`listening on port ${conf.env.port}`);
+      return {
+        app,
+        port: conf.env.port,
+        router
+      };
+    };
   }
 });
